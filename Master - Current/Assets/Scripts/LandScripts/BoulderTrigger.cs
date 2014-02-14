b734bd1c7d711c79292f9fe5fbb5914974b4b2f6
kaladbolgII <sly.keyboard@gmail.com>
2014-02-14 13:44:30 +0000

    Master ver. 1.2.1
    -Deleted the deer action

--- Master - Current/Assets/Scripts/LandScripts/BoulderTrigger.cs
+++ Master - Current/Assets/Scripts/LandScripts/BoulderTrigger.cs
@@ -12,21 +12,11 @@ public class BoulderTrigger : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetButtonDown ("Fire1"))
-			rigidbody2D.mass = 0;
-		else
-			rigidbody2D.mass = 3000;
+	
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		if (other.gameObject.name == "Player") 
-		{
-			if(Player.GetForm() == 1)
-			{
-
-			}
-		}
 		// Triggers if collides with the player
 		if (other.gameObject.collider2D.name == "BreakableFloor")
 		{
