1c71c4817ef92753049942a4187f4fa589613741
Fnaaarp <1100223@live.abertay.ac.uk>
2014-02-21 13:42:21 +0000

    Interaction Test 4

---
+++ Master - Current/Ben's Interaction Code/Assets/Scripts/WaterScripts/WaterBound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterBound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerStay2D(Collider2D obj)
+	{
+		if (obj.gameObject.name == "Player" && Player.GetForm() == 2)
+		{
+			Player.SetSwimming(true);
+			obj.rigidbody2D.gravityScale = 1;
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D obj)
+	{
+		if (obj.gameObject.name == "Player")
+		{
+			Player.SetSwimming(false);
+			obj.rigidbody2D.gravityScale = 15;
+		}
+	}
+}
