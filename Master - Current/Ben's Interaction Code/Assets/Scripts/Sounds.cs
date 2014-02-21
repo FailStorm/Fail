c779dd1250360095b3d856591280c772671706bd
Fnaaarp <1100223@live.abertay.ac.uk>
2014-02-21 13:41:59 +0000

    Interaction Test 3

---
+++ Master - Current/Ben's Interaction Code/Assets/Scripts/Sounds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sounds : MonoBehaviour {
+
+	static Sounds s;
+
+	// Use this for initialization
+	void Start () {
+		s = GetComponent<Sounds>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//
+	}
+
+	public static void Stop()
+	{
+		s.audio.volume = 0.5f;
+	}
+}
