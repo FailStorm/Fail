5ce5bab032d932f542f99a4910f4b8463af926cd
Fnaaarp <1100223@live.abertay.ac.uk>
2014-02-21 13:41:36 +0000

    Interaction Test 2

---
+++ Master - Current/Ben's Interaction Code/Assets/Scripts/Airjet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Airjet : MonoBehaviour 
+{
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		// Triggers if collides with the player
+		if (other.gameObject.collider2D.name == "Player")
+		{
+			if (this.gameObject.name == "AirJet")
+			{
+				other.gameObject.rigidbody2D.AddForce(new Vector2(0, 300000000 * Time.deltaTime));
+			}
+		}
+	}
+}
---
+++ Master - Current/Ben's Interaction Code/Assets/Scripts/LandScripts/BoulderTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderTrigger : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		// Triggers if collides with the player
+		if (other.gameObject.collider2D.name == "BreakableFloor")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
