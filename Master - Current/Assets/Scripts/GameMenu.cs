630032372a9fe7d41c6b90ab17a3fec460bb474d
Forrow <forrow@hotmail.com>
2014-02-24 16:37:14 +0000

    Menu Additions.
    Added placeholder menu assets. Start menu and game menu linked. P key
    pauses in game and brings up temporary menu.

---
+++ Master - Current/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+
+	bool paused = false;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if(!paused) 
+			{
+				Time.timeScale = 0;
+				paused = true;
+				Pause ();
+			} 
+			else 
+			{
+				Time.timeScale = 1;
+				paused = false;
+			}
+		}
+	}
+
+	void Pause()
+	{
+		OnGUI();
+	}
+
+	void OnGUI(){
+	
+		if (paused == true)
+		{
+			if (GUI.Button (new Rect (0, 0, 150, 50), "Main Menu")) {
+				Application.LoadLevel(0);
+				paused = false;
+			}
+	
+			if (GUI.Button (new Rect (0, 100, 150, 50), "Settings")) {
+				
+			}
+			
+			if (GUI.Button (new Rect (0, 200, 150, 50), "About")) {
+				
+			}
+			
+			if (GUI.Button (new Rect (0, 300, 150, 50), "Credits")) {
+				
+			}
+			
+			if (GUI.Button (new Rect (0, 400, 150, 50), "Exit Game")) {
+				Application.Quit();
+			}
+		}
+	}
+
+}
---
+++ Master - Current/Assets/Scripts/Menu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	
+	public GUIStyle PlayStyle, AboutStyle, CreditsStyle, SettingsStyle;
+	public float volumeLevel = 10.0f;
+	bool settings = false;
+
+	void OnGUI(){
+
+		
+
+		//BeginArea(Rect screenRect, GUIStyle style);
+
+		GUILayout.BeginArea (new Rect (210, 225, 100, 100));
+		if (GUILayout.Button ("", PlayStyle)) 
+		{
+			Application.LoadLevel(1);
+		}
+		GUILayout.EndArea ();
+
+		GUILayout.BeginArea (new Rect (285, 175, 100, 100));
+		if (GUILayout.Button ("", AboutStyle)) 
+		{
+		}
+		GUILayout.EndArea ();
+
+		GUILayout.BeginArea (new Rect (380, 180, 100, 100));
+		if (GUILayout.Button ("", CreditsStyle)) 
+		{
+		}
+		GUILayout.EndArea ();
+
+		GUILayout.BeginArea (new Rect (170, 320, 100, 100));
+		if (GUILayout.Button ("", SettingsStyle)) 
+		{
+			settings = true;
+		}
+		GUILayout.EndArea ();
+
+		if (settings)
+		{
+			volumeLevel = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), volumeLevel, 10.0F, 20.0F);
+			
+			if (GUI.Button (new Rect (25, 50, 100, 50), "Done")) 
+			{
+				settings = false;
+			}
+			
+			//audio.volume = volumeLevel *0.2; Doesn't go in here btw
+		}
+
+
+	}
+}
