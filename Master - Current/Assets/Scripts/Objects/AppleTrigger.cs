8d8b78ef3db0f591c31ec89e15349f92b73b5ae4
SoberMilk <sobermilk@googlemail.com>
2014-02-26 16:57:17 +0000

    Master ver 1.4
    CHANGELOOOOOG:
    -Cleaned up alot
    -'Death' implemented
    -Changing from Orb to Deer on death implemented
    -Mushrooms
    -Apples
    -Airjet
    -Layers all correct
    -Boulder (alpha)
    -Bens other scripts imported (but not implemented)

---
+++ Master - Current/Assets/Scripts/Objects/AppleTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Master - Current/Assets/Scripts/Player/PlayerAir.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAir : MonoBehaviour
+{
+	private static float xSpeed = 70;
+	private static float ySpeed = 30;
+	
+	// Handles movement
+	public static void Move (Rigidbody2D body, bool grounded)
+	{
+		Vector2 movement;
+		movement.x = Input.GetAxisRaw ("Horizontal") * xSpeed * 500 * Time.deltaTime;
+		movement.y = 0;
+		
+		// Jumping
+		//if (grounded) // If jump is pressed and the player is not falling / rising...
+		// If jump is pressed and the player is not falling / rising...
+		if (Input.GetButtonDown ("Jump"))
+		{
+			/*if (Input.GetButtonDown ("Jump"))
+			{
+				movement.y = ySpeed;
+			}*/
+			movement.y = ySpeed * 500;
+		}/*
+		else
+		{
+			if (Input.GetButtonDown ("Jump"))
+			{
+				movement.y = ySpeed / 2;
+			}
+			
+			body.drag = 2;
+			
+			if(body.velocity.y <= -3 && Input.GetButton ("Jump"))
+			{
+				body.drag = 6;
+				body.AddForce(new Vector2(0, 500));
+			}
+		}*/
+		if (!Player.swimming)
+			//Gliding
+			if (!grounded)
+		{
+			if(body.velocity.y <= -3 && Input.GetButton ("Jump"))
+			{
+				//body.drag = 5;
+				body.AddForce(new Vector2(0, 500));
+			}
+		}
+		
+		
+		// Add forces		
+		if (movement.x != 0 && body.velocity.x <= 20 && body.velocity.x >= -20)
+		{
+			body.AddForce(new Vector2(movement.x, 0));		
+		}
+		
+		if (movement.y != 0)
+		{
+			body.AddForce(new Vector2(0, movement.y));
+		}
+	}
+}
